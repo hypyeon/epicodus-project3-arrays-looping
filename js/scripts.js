7e95017b0b65826f33fb004ecca45f66d365c0d5
Hayeong Pyeon <hayeong@HayeongcBookAir.home>
2023-11-18 15:37:25 -0800

    add styling on SCSS & formHandler on JS

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,18 @@ function getArray(v) {
         return `Please provide a positive integer.`;
     }
 };
+
+function formHandler() {
+    const form = document.querySelector("form");
+    const result = document.getElementById("result");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        result.classList.remove("hidden");
+    });
+    form.addEventListener("reset", () => {
+        result.classList.add("hidden");
+    });
+};
+window.onload = () => {
+    formHandler();
+}
