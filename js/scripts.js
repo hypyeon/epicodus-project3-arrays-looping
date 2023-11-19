a6e4101985903cc67670c6938e33ef2e5a80cf76
Hayeong Pyeon <hayeong@HayeongcBookAir.home>
2023-11-18 16:17:10 -0800

    add second function on JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//business logic:
 function getArray(v) {
     const numArr = [];
     if (Number.isInteger(v) && v >= 0) {
@@ -9,13 +10,39 @@ function getArray(v) {
         return `Please provide a positive integer.`;
     }
 };
+function replaceNumToPhrase(v) {
+    const numArr = getArray(v);
+    const arrLen = numArr.length;
+    const one = "1";
+    const two = "2";
+    const three = "3";
+    for (let i = 0; i <= arrLen; i++) {
+        const currentNum = numArr[i].toString();
+        if (currentNum.includes(one)) {
+            numArr[i] = "Beep!";
+        } else if (currentNum.includes(two)) {
+            numArr[i] = "Boop!";
+        } else if (currentNum.includes(three)) {
+            numArr[i] = "Won't you be my neighbor?";
+        } else {
+            return;
+        }
+    }
+    return numArr;
+}
 
+//UI logic: 
 function formHandler() {
     const form = document.querySelector("form");
     const result = document.getElementById("result");
+    const numValue = document.getElementById("number").value;
+    const num = parseInt(numValue);
+    const array = replaceNumToPhrase(num);
+
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         result.classList.remove("hidden");
+        result.innerText = array;
     });
     form.addEventListener("reset", () => {
         result.classList.add("hidden");
